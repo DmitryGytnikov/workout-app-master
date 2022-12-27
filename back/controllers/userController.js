8e45a3c9bddcd95e09ac06c438d4dfaa0c06ac4b
DmitryGytnikov <gytnikovd@mail.ru>
2022-12-27 23:25:09 +0300

    Video 4:
    Add comments
    Install and connect (config/db.js) mongoDB, mongoosh, compass
    Add userModel.js (with bcryptjs)

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,11 +1,12 @@
-// @desc    Get user profile
-// @route   GET /api/users/profile
-// @access  Private
+// @desc    Get user profile Описание
+// @route   GET /api/users/profile Путь
+// @access  Private Доступ - авторизованный пользователь
 export const getUserProfile = (req, res) => {
+	// Получаем всех юзеров
 	const user = {
 		name: 'Max',
 		age: 21,
 	}
 
-	res.json(user)
+	res.json(user) // Уже включен статус 200, не нужно его писать
 }
--- back/server.js
+++ back/server.js
@@ -1,26 +1,32 @@
-import express from 'express'
-import morgan from 'morgan'
+import express from 'express' // Помогает сервер писать
+import morgan from 'morgan' // Отображение в терминале всех запросов
 import dotenv from 'dotenv'
-import colors from 'colors'
+import colors from 'colors' // Подсветка в консоли console.log
+
+/* Config */
+import { connectDB } from './config/db.js'
 
 /* Routes */
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js' // Для бэка .js обязательно  писать
 
-dotenv.config()
+dotenv.config() // Чтобы везде работали переменные среды из .env
+
+connectDB() // Для коннекта БД
 
 const app = express()
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
-app.use(express.json())
+app.use(express.json()) // Настройки экспресс- ответ в json
 
-app.use('/api/users', userRoutes)
+/* Инициализация */
+app.use('/api/users', userRoutes) // путь, роут
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold // NODE_ENV - состояние проекта
 	)
 )
