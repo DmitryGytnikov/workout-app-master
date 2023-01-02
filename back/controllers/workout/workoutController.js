7c86259331dbe44c4b6607565d391d46d2720284
DmitryGytnikov <gytnikovd@mail.ru>
2023-01-02 23:05:09 +0300

    Video 10: Get workout with list exercises with calc minute
    Add controller in workoutController.js
    Add route
    +  testing Insomnia

--- back/controllers/workout/workoutController.js
+++ back/controllers/workout/workoutController.js
@@ -16,3 +16,17 @@ export const addNewWorkout = asyncHandler(async (req, res) => {
 
 	res.json(workout)
 })
+
+// @desc    Get workout
+// @route   GET /api/workouts/:id
+// @access  Private
+export const getWorkout = asyncHandler(async (req, res) => {
+	//Получаем workout
+	const workout = await Workout.findById(req.params.id)
+		.populate('exercises') // Чтобы раскрылось содержимое упражнений
+		.lean() // Для нормального отображения ...workout в res.json
+
+	const minutes = Math.ceil(workout.exercises.length * 3.7)
+
+	res.json({ ...workout, minutes })
+})
--- back/routes/workoutRoutes.js
+++ back/routes/workoutRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { addNewWorkout } from '../controllers/workout/workoutController.js'
+import { getWorkout } from '../controllers/workout/workoutController.js'
+
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, addNewWorkout)
 //запрос  на адрес /, исп-ть логику addNewWorkout, и пользователь д.б. авторизован
+router.route('/:id').get(protect, getWorkout)
 
 export default router
